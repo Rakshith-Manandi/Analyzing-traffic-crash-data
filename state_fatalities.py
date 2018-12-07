0bb84271654cf9c17c04593cbcafc8fe74683f7d
Rakshith  Manandi <rmanandi@eng.ucsd.edu>
2018-12-07 09:27:55 -0800

    Refined version of story1

---
+++ state_fatalities.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+def readFile(x):
+	output = pd.read_csv(x)
+	return output
+
+stcodes = np.asarray(['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', \
+	                     'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', \
+	                     'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', \
+	                     'VA', 'WA', 'WV', 'WI', 'WY'])
+
+stpopulation = np.asarray([4858979, 738432, 6828065, 2978204, 39144818, 5456574, 3590886, 945934, 646449, 20271272, 10214860, 1431603, 1654930, 12859995, 6619680, \
+	                           3123899, 2911641, 4425092, 4670724, 1329328, 6006401, 6794422, 9922576, 5489594, 2992333, 6083672, 1032949, 1896190, 2890845, \
+	                           1330608, 8958013, 2085109, 19795791, 10042802, 756927, 11613423, 3911338, 4028977, 12802503, 1056298, 4896146, 858469, 6600299, \
+	                           27469114, 2995919, 626042, 8382993, 7170351, 1844128, 5771337, 586107])
+
+def by_state(x):
+	states = x.groupby('STATE')	
+	count_by_state = np.asarray(states.YEAR.count())
+	global stcodes
+	return stcodes, count_by_state
+
+def by_state_per_capita(x):
+	states = x.groupby('STATE')
+	count_by_state = np.asarray(states.YEAR.count())
+	global stcodes, stpopulation
+	acc_per_capita = count_by_state / stpopulation * 1e+5
+	return stcodes, acc_per_capita
